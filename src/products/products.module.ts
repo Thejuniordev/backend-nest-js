011beb22a7a0aa77a4bf6ec95610c69939505a92
Darcy Junior <darcy.santos@compasso.com.br>
2020-11-25 09:56:02 -0300

    Create products module

---
+++ src/products/products.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ProductsController } from './products.controller';
+import { ProductService } from './shared/product.service';
+
+@Module({
+    controllers: [ProductsController],
+    providers: [ProductService],
+})
+export class ProductsModule {}
